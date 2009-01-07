ed4eea34115ad520c41dd781843a6d696a3691b7
alexandre.bodelot <alexandre.bodelot@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-01-07 14:14:18 +0000

    Quelques petites optimisations dans Music - utilisation de dumb_load_*_quick (pas besoin de seeking) - format détecté à l'extension - virtual inutiles supprimées

--- trunk/Music.hpp
+++ trunk/Music.hpp
@@ -9,22 +9,21 @@
 
 #define SAMPLING_RATE  44100
 #define N_CHANNELS     2
-#define DELTA          65536.0f / SAMPLING_RATE
 #define BUFFER_LENGTH  1	// Fixme: Magique?
 #define BUFFER_SIZE    SAMPLING_RATE / BUFFER_LENGTH
 
 /**
- * Musique chargée avec libdumb (support du format .mod)
+ * Musique chargée avec libdumb (.mod, .xm, .s3m, .it)
  */
 class Music : public sf::SoundStream
 {
 public:
-	Music(const char* name);
+	Music(const char* filename);
 	~Music();
 	
-	virtual bool OnStart();
-
-	virtual bool OnGetData(Chunk& data);
+	bool OnStart();
+	
+	bool OnGetData(Chunk& data);
 	
 private:
 	sf::Int16 samples_[BUFFER_SIZE * N_CHANNELS];

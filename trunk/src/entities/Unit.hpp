c294afd92ee5ff8279c4e2ae251c1cbf2ac30604
alexandre.bodelot <alexandre.bodelot@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-06 21:16:01 +0000

    * Ajout d'un effet graphique lorsqu'on perd des points de vie : le sprite se colore progressivement en rouge puis recouvre son aspect d'origine marche sur toutes les unités Appuyez sur F2 pour tester sur le joueur

--- trunk/src/entities/Unit.hpp
+++ trunk/src/entities/Unit.hpp
@@ -9,9 +9,13 @@ class Unit: public Entity, public Animated
 {
 public:
 	Unit(const sf::Vector2f& pos, const sf::Image& image);
-
+	
+	// cf. Entity
 	void Update(float frametime) = 0;
-
+	
+	// cf. Entity
+	void TakeDamage(int damage);
+	
 	void SetHP(int hp);
 
 	void SetAnimation(Direction dir, const Animation* anim);
@@ -20,6 +24,14 @@ protected:
 	// Animations de déplacement
 	const Animation* walk_anims_[COUNT_DIRECTION];
 	int hp_;
+
+private:
+	enum Bleeding
+	{
+		BLEED_IN, BLEED_OUT, BLEED_STOP
+	};
+	Bleeding bleeding_;
+	float bleed_timer_;
 };
 
 

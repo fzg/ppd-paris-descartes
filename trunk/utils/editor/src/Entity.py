b89b3b27c43b4c58c925153a7bd3225710a85be9
alexandre.bodelot <alexandre.bodelot@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-06 02:57:12 +0000

    * éditeur python réparé (supporte le dernier format de map) ajout de boîtes de dialogue pour lister les entités et les téléporteurs de la zone à l'écran (menu "Voir les entités" et "Voir les téléporteurs")
    * suppression de tiles redondantes dans le tileset, quelques retouches

--- trunk/utils/editor/src/Entity.py
+++ trunk/utils/editor/src/Entity.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 
+
 class Entity:
 	"entité d'une zone"
 	
-	def __init__(self, name, x, y):
-		self.name = name
+	def __init__(self, id_, x, y):
+		self.id = id_
 		self.x = x
 		self.y = y
 	
 	def __lt__(self, other):
-		return self.name < other.name
+		return self.id < other.id
 	
 	def __str__(self):
-		return "%s (x: %d, y: %d)" % (self.name, self.x, self.y)
+		return "entité id %d (x: %d, y: %d)" % (self.id, self.x, self.y)
 	
 	def to_xml(self):
-		return '<entity name="%s" x="%d" y="%d" />' % (self.name, self.x, self.y)
+		return '<entity id="%d" x="%d" y="%d" />' % (self.id, self.x, self.y)
 		
--- trunk/utils/editor/src/Teleporter.py
+++ trunk/utils/editor/src/Teleporter.py
@@ -1,21 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import xml.dom.minidom as dom
 
 class Teleporter:
 	"téléporteur d'une zone"
 	
-	def __init__(self, xml_tp):
+	def __init__(self, container, x, y, zone_x, zone_y, tile_x, tile_y):
 
-		self.container = int(xml_tp.getAttribute("container"))
-		self.x = int(xml_tp.getAttribute("x"))
-		self.y = int(xml_tp.getAttribute("y"))
-		self.zone_x = int(xml_tp.getAttribute("zone_x"))
-		self.zone_y = int(xml_tp.getAttribute("zone_y"))
-		self.tile_x = int(xml_tp.getAttribute("tile_x"))
-		self.tile_y = int(xml_tp.getAttribute("tile_y"))
+		self.container = container
+		self.x = x
+		self.y = y
+		self.zone_x = zone_x
+		self.zone_y = zone_x
+		self.tile_x = tile_x
+		self.tile_y = tile_y
+	
+	def __str__(self):
+		return "tp en [%d][%d], à destination de la map %d, zone [%d][%d], tile [%d][%d]" % (self.y, self.x, self.container, self.zone_y, self.zone_x, self.tile_y, self.tile_x)
 	
 	def to_xml(self):
 		return '<tp x="%d" y="%d" container="%d" zone_x="%d" zone_y="%d" tile_x="%d" tile_y="%d" />' % \
 			(self.x, self.y, self.container, self.zone_x, self.zone_y,
 			self.tile_x, self.tile_y)
+

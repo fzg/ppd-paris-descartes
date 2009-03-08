e23e743a431a3cbbd8ea6c62384f33908560de97
alice.itouch <alice.itouch@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-08 20:27:06 +0000

    *Implémentation partielle du système de label (il reste le rendu) *Ajout d'un namespace gui (la dernière révision l'avait aussi) *Amélioration de la gestion XML dans le gui_system On peux lire pour l'instant deux types de widget : Button et Label dont la syntaxe est la suivante :
    <button id="identifiant du boutton (int)" pic="image à charger" x="position x (int)" y="position y (int)" />
    e.g:	<button id="0" pic="button_prev2" x="20" y="20" />
    <label x="position x(int)" y="position y(int)" >le texte à écrire dans le label</label>
    e.g:    <label x="10" y="100" >texte</label>
    
    Un élément à été ajouté, il s'agit de Window :
    <window background="test.png" x="10" y="20" w="800" h="600" />
    mais sa syntaxe va très probablement changer.
    
    *Mise à jour du fichier XML avec le nouveau système

--- trunk/src/gui_system/Label.cpp
+++ trunk/src/gui_system/Label.cpp
@@ -1,15 +1,18 @@
-#ifdef _DEBUG
 #include <iostream>
-#endif
 
 #include "Label.hpp"
 
 using namespace gui;
+using namespace std;
 
-Label::Label() : Control(){
-
+Label::Label(ControlPos pos, const std::string& text) : Control(0, pos){
+    cout << "Creation d'un label " << text << endl;
 }
 
 Label::~Label(){
+
+}
+
+void Label::Show(sf::RenderTarget& app){
 
 }

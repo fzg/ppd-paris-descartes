7e3db73a92c878409687baf6543267c673f0120f
alice.itouch <alice.itouch@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-08 21:34:14 +0000

    *Ajout de nouveaux médias pour l'interface (médias de tests) *Le système d'event sur les boutons est totalement fonctionnel : Le callback permet de récupérer l'id du bouton cliqué sur la fenêtre MyWin. *Implémentation du système de label. Il est maintenant possible d'écrire du texte dans une fenêtre *Changement de l'écriture XML des fenêtres : L'élément "controls" à été changé par "window" et ce dernier contients les attributs de la fenêtre (position, image de fond, ...) *Ajout d'un attribut alpha pour permettre de rendre la fenêtre transparente
    /!\ Les widgets ne dépendent pas encore de la fenêtre. Plus tard leurs positions seront calculés en fonction de la position de la fenêtre mère pour plus de clarté.
    Il est possible d'ajouté un système de déplacement de la fenêtre mais il n'est pas prioritaire.
    A terme le système de fenêtre remplacera le HUD actuel, et permettra l'accès aux options ainsi qu'au menu principal.

--- src/gui_system/Button.cpp
+++ src/gui_system/Button.cpp
@@ -12,16 +12,14 @@ Button::Button() : Control(){
 
 Button::Button(ControlID id, ControlPos pos, const std::string& str) : Control(id, pos){
     img_ = GET_IMG(str.c_str());
-    //ControlPos p = img_.GetSize();
 
-    cout << "Image size : " << img_.GetSize().x;
+    img_.SetPosition(pos.x, pos.y);
 
     // Positionnement du contrôle
-    /*rect_.Top = pos.y;
-    rect_.Left = pos.x;
-    rect_.Bottom = pos.y + p.y;
-    rect_.Right = pos.x + p.x;
-    */
+    rect_.Top = (int)pos.y;
+    rect_.Left = (int)pos.x;
+    rect_.Bottom = (int)pos.y + img_.GetSize().y;
+    rect_.Right = (int)pos.x + img_.GetSize().x;
 }
 
 void Button::Show(sf::RenderTarget& app){
--- src/gui_system/Label.cpp
+++ src/gui_system/Label.cpp
@@ -6,7 +6,14 @@ using namespace gui;
 using namespace std;
 
 Label::Label(ControlPos pos, const std::string& text) : Control(0, pos){
-    cout << "Creation d'un label " << text << endl;
+    if(!font_.LoadFromFile("data/font/VeraMono.ttf")){
+        cerr << "Impossible de charger la font" << endl;
+    }
+    text_.SetText(text);
+    text_.SetFont(font_);
+    text_.SetSize(12);
+
+    text_.Move(pos.x, pos.y);
 }
 
 Label::~Label(){
@@ -14,5 +21,5 @@ Label::~Label(){
 }
 
 void Label::Show(sf::RenderTarget& app){
-
+    app.Draw(text_);
 }

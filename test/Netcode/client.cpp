a9d133f8c96d857f4b26c49ae173400db5aa71e3
alice.itouch <alice.itouch@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-08 22:13:48 +0000

    *Publication des essais sur le netcode Le système décrit comment envoyer des données ou en recevoir à partir de classes.

---
+++ test/Netcode/client.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <SFML/Network.hpp>
+
+#include "decl.hpp"
+
+#define SPORT 4567
+
+using namespace std;
+
+int main(){
+    Player p(2, 1);
+    sf::SocketUDP Socket;
+    sf::Packet sSend;
+    sf::IPAddress dest("127.0.0.1");
+
+    sSend << p;
+
+    cout << "Envoi des donnees ..." << endl;
+    if(Socket.Send(sSend, dest, SPORT) != sf::Socket::Done){
+        cerr << "Impossible d'envoyer les données" << endl;
+        return 1;
+    }
+    /*
+    char buffer[] = "hello world";
+    if(Socket.Send(buffer, sizeof(buffer), dest, SPORT) != sf::Socket::Done){
+        cerr << "Impossible d'envoyer les données" << endl;
+        return 1;
+    }
+    */
+
+    return 0;
+}

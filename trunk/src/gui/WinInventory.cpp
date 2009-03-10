762c84cfec58ecd384f741cc3df1603664809844
alice.itouch <alice.itouch@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-10 14:47:56 +0000

    *Amélioration de l'inventaire. WinInventory à maintenant presque le même comportement qu'Inventory.

--- trunk/src/gui/WinInventory.cpp
+++ trunk/src/gui/WinInventory.cpp
@@ -3,22 +3,46 @@
 #include "WinInventory.hpp"
 #include "../entities/Equipment.hpp"
 
-#define OFFSET_X ((640 - (WIDTH * SLOT_SIZE)) / 2.0)
-#define OFFSET_Y ((480 - (HEIGHT * SLOT_SIZE)) / 2.0)
+#include "../gui_system/button.hpp"
 
 using namespace gui;
 using namespace std;
 
-WinInventory::WinInventory(){
+WinInventory::WinInventory()
+{
     /* Chargement du descriptif XML */
     Load("data/window/inventory.xml");
+
+    last_item_ = 0;
+    items_ = new Equipment*[ITEM_N+1];
 }
 
-void WinInventory::AddItem(Equipment* item){
-    cout << "Add item to inventory" << endl;
+WinInventory::~WinInventory()
+{
+    delete[] items_;
+
+    UnLoad();
 }
 
-int WinInventory::WindowCallback(const Control::ControlID id, const int p1, void *p2){
+int WinInventory::AddItem(Equipment* item)
+{
+    if(last_item_ > ITEM_N){
+        puts("inventaire plein");
+        return 1;
+    }
+    Control *c = GetFromID(last_item_+ID_SLOT);
+    if(c == NULL){
+        puts("mauvaise id");
+        return 0;
+    }
+
+    c->ChangeSprite(*item);
+    items_[last_item_] = item;
+    last_item_ ++;
+}
+
+int WinInventory::WindowCallback(const Control::ControlID id, const int p1, void *p2)
+{
     cout << "Inventory callback" << endl;
     /*
     switch(id){

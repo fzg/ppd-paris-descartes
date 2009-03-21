696b969fe755dd8876b972332c9c61669255364c
alice.itouch <alice.itouch@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-03-21 13:17:19 +0000

    *Préparation du système de Script
    NOTE: Il va falloir créer une entitée fixe qui n'aggresse pas le joueur. Un npc neutre.
    Je lui associerait un dialogue.

---
+++ src/core/Script.cpp
@@ -0,0 +1,19 @@
+#include "Script.hpp"
+
+using namespace std;
+
+Script::Script()
+{
+
+}
+
+Script::~Script()
+{
+
+}
+
+void Script::Load(const std::string & str)
+{
+
+
+}
---
+++ src/gui/Dialog.hpp
@@ -0,0 +1,14 @@
+#ifndef DIALOG_HPP
+#define DIALOG_HPP
+
+#include "../gui_system/window.hpp"
+
+class Dialog : public gui::Window{
+public:
+    Dialog();
+
+private:
+    int WindowCallback(const gui::Control::ControlID id, const int p1, void *p2);
+};
+
+#endif

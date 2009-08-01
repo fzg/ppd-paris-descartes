25bd071fd08b45d271db41c8d2ee72a700e2b7a7
frozensys <frozensys@0da0bcb4-d257-11dd-801b-4930de48a34e>
2009-08-01 00:55:58 +0000

    -> Modifs de la classe LoadingWindow 	(threadée, refresh automatique, "singleton destructible") TODO: Progressbar (les noms des fichiers défileraient trop vite)
    WARNING: Après avoir appelé la methode Dismiss, il peut être hasardeux de 
    réutiliser des références pointant sur le singleton...
    
    Exemple:
    LoadingWindow &lw = LoadingWindow::GetInstance();
    lw.Dismiss();
    lw.SetStatusMessage("Boum");
    
    NB: Je m'attendais à ce que ça pête, mais non. Le destructeur est pourtant 
    appelé. Il faudrait approfondir un peu...
    AW

--- trunk/src/misc/LoadingWindow.hpp
+++ trunk/src/misc/LoadingWindow.hpp
@@ -2,26 +2,35 @@
 #define LOADING_WINDOW_H
 
 #include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
 
 /**
  * Splash-Screen / Bugfix mac
  */
-class LoadingWindow
+class LoadingWindow : private sf::Thread
 {
 public:
 	static LoadingWindow& GetInstance();
 	void SetMessageString(const char* msg);
-	void Render();
+
 	void Dismiss();
 
 private:
-		LoadingWindow();
-		~LoadingWindow();
+	LoadingWindow();
+	~LoadingWindow();
+	
+	virtual void Run() {
+		window_->Clear();
+		sf::Sleep(0.1f);
+		window_->Draw(*message_);
+		window_->Display();
+	};
+	
+	sf::String* message_;
+	sf::RenderWindow* window_;
 
-		sf::String message_;
-		sf::RenderWindow window_;
-		bool initialized_;
-		LoadingWindow* address_;
+protected:
+	LoadingWindow* address_;
 };
 
 #endif // LOADING_WINDOW_H
